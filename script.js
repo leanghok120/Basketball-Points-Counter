ada5a84c7e4e4cfd57f8b38f2c5ec038648995b2
leanghok120 <leanghok@DESKTOP-B4IDCRT>
2024-03-29 09:43:21 +0700

    Make the buttons increase points when clicked

---
+++ script.js
@@ -0,0 +1,16 @@
+let home = document.getElementById("home")
+let guest = document.getElementById("guest")
+let pointsHome = 0;
+let pointsGuest = 0;
+
+function increasePoints(amount, team) {
+  if (team === "home") {
+    pointsHome += amount
+    home.textContent = pointsHome 
+  } else if (team === "guest") {
+    pointsGuest += amount
+    guest.textContent = pointsGuest
+  } else {
+    console.log("Team not found.")
+  }
+}
