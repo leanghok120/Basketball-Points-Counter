b1868700b89f53057ca87693af77b5b53937418f
leanghok120 <leanghok@DESKTOP-B4IDCRT>
2024-03-29 09:58:22 +0700

    Add border to container

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let home = document.getElementById("home")
 let guest = document.getElementById("guest")
-let pointsHome = 0;
-let pointsGuest = 0;
+let pointsHome = 0
+let pointsGuest = 0
 
 function increasePoints(amount, team) {
   if (team === "home") {
